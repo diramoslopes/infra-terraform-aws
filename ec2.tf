9a63edddf64c9299dd45d639de559bb669857af9
Diogo <diogo322008@hotmail.com>
2025-06-30 20:16:34 -0300

    criação arquivo provider.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,3 @@
-provider "aws" {
-  region = var.aws_region
-}
-
-
 resource "aws_instance" "web_dimais" {
   ami           = "ami-0c803b171269e2d72"
   instance_type = "t2.micro"
--- s3.tf
+++ s3.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = var.aws_region
-}
-
 resource "aws_s3_bucket" "dimais" {
   bucket = "my-tf-test-bucket-dimais"
   tags   = var.tags
