371d1b787641331c3e49178d26ac158198d86d32
Diogo <diogo322008@hotmail.com>
2025-06-30 20:20:18 -0300

    ajuste names

--- variables.tf
+++ variables.tf
@@ -8,7 +8,7 @@ variable "tags" {
   description = "The name tag for the S3 bucket."
   type        = map(string)
   default = {
-    Name        = "My bucket Dimais"
+    Name        = "My Dimais"
     Environment = "Dev"
     Owner       = "Diogo Silveira"
     Updated     = "2025-06-30"
